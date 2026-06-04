4f04a87fb8afd9b45070d611dda3f8ac894a9d8a
Vitora OS <emmykarl07@gmail.com>
2026-06-04 08:37:54 +0100

    Implement encryption and decryption functions

---
+++ src/services/crypto.js
@@ -0,0 +1,27 @@
+import crypto from 'crypto'
+
+const ALGORITHM = 'aes-256-cbc'
+const KEY = Buffer.from(process.env.ENCRYPTION_KEY, 'utf8').slice(0, 32)
+
+export function encrypt(text) {
+  const iv = crypto.randomBytes(16)
+  const cipher = crypto.createCipheriv(ALGORITHM, KEY, iv)
+  const encrypted = Buffer.concat([
+    cipher.update(text, 'utf8'),
+    cipher.final()
+  ])
+  // Store iv + encrypted together so we can decrypt later
+  return iv.toString('hex') + ':' + encrypted.toString('hex')
+}
+
+export function decrypt(stored) {
+  const [ivHex, encryptedHex] = stored.split(':')
+  const iv = Buffer.from(ivHex, 'hex')
+  const encrypted = Buffer.from(encryptedHex, 'hex')
+  const decipher = crypto.createDecipheriv(ALGORITHM, KEY, iv)
+  const decrypted = Buffer.concat([
+    decipher.update(encrypted),
+    decipher.final()
+  ])
+  return decrypted.toString('utf8')
+}
